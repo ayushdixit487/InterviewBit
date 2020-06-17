628c25c97766baa3231f832530f9d3c6d19439aa
Ayush Dixit <ayushdixit661@gmail.com>
2020-06-17 08:15:44 +0530

    Create KReverseLinkedList.cpp

---
+++ Linked-Lists/KReverseLinkedList.cpp
@@ -0,0 +1,46 @@
+ListNode* Solution::reverseList(ListNode* A, int B) {
+    if(!A || B == 1 || B == 0){
+        return A;
+    }
+    
+    ListNode* head = NULL, *curr = A, *prev = NULL, *tempHead = NULL;
+    
+    for(int i = 0; i < B; i++){
+        ListNode *temp = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = temp;
+        
+        if(tempHead == NULL){
+            tempHead = prev;
+        }
+    }
+    
+    head = prev;
+
+    prev = NULL;
+    
+    while(curr){
+        ListNode* t = NULL;
+        
+        for(int i = 0; i < B; i++){
+            ListNode *temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+            
+            if(!t){
+                t = prev;
+            }
+        }
+        
+        tempHead->next = prev;
+        tempHead = t;
+    }
+    
+    if(tempHead){
+        tempHead->next = NULL;
+    }
+    
+    return head;
+}
