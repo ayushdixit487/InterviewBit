60f69af0066a2ded2b6eb7971a2cfba1ecd0ce89
Ayush Dixit <ayushdixit661@gmail.com>
2020-06-17 08:11:58 +0530

    Create InsertionSortList.cpp

---
+++ Linked-Lists/InsertionSortList.cpp
@@ -0,0 +1,50 @@
+ListNode* Solution::insertionSortList(ListNode* A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    
+    if(A == NULL){
+        return A;
+    }
+    
+    ListNode* curr = A->next;
+    ListNode* temp;
+    ListNode* prev = A;
+    ListNode* head = A;
+    ListNode* check;
+    ListNode* checkPrev;
+    
+    while(curr != NULL){
+        temp = curr->next;
+        if(curr->val < prev->val){
+            check = head;
+            checkPrev = NULL;
+            while(check != curr){
+                if(check->val > curr->val){
+                    break;
+                }
+                checkPrev = check;
+                check = check->next;
+            }
+            if(checkPrev == NULL){
+                head = curr;
+                curr->next = check;
+                prev->next = temp;
+                curr = temp;
+            }
+            else{
+                checkPrev->next = curr;
+                curr->next = check;
+                prev->next = temp;
+                curr = temp;
+            }
+        }
+        else{
+            prev = curr;
+            curr = temp;
+        }
+    }
+    
+    return head;
+}
