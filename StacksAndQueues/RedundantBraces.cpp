1c709ffe8d3d3c8059e9de7e781dbb87dd8ab397
Ayush Dixit <ayushdixit661@gmail.com>
2020-06-20 21:01:27 +0530

    Create RedundantBraces.cpp

---
+++ StacksAndQueues/RedundantBraces.cpp
@@ -0,0 +1,36 @@
+/*
+Write a program to validate if the input string has redundant braces?
+Return 0/1 
+ 0 -> NO 1 -> YES 
+Input will be always a valid expression
+and operators allowed are only + , * , - , /
+Example:
+((a+b)) has redundant braces so answer will be 1
+(a + (a + b)) doesn't have have any redundant braces so answer will be 0
+https://www.interviewbit.com/problems/redundant-braces/
+*/
+
+int Solution::braces(string A) {
+    stack<char> s;
+    auto size = A.length();
+    auto i = 0;
+    while(i<size)
+    {
+        char c = A[i];
+        if (c == '(' || c == '+' || c == '*' || c == '-' || c == '/')
+            s.push(c);
+        else if (c == ')')
+        {
+            if (s.top() == '(')
+                return 1;
+            else
+            {
+                while (!s.empty() && s.top() != '(')
+                    s.pop();
+                s.pop();
+            }
+        }
+        ++i;
+    }
+    return 0;
+}
