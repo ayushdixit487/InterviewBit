16f376ce113f6331c7fa790a3071165c933ebeba
Ayush Dixit <ayushdixit661@gmail.com>
2020-06-20 21:02:36 +0530

    Create ReverseString.cpp

---
+++ StacksAndQueues/ReverseString.cpp
@@ -0,0 +1,20 @@
+/*
+Given a string S, reverse the string using stack.
+Example :
+Input : "abc"
+Return "cba"
+https://www.interviewbit.com/problems/reverse-string/
+*/
+
+string Solution::reverseString(string A) {
+    stack<char> s;
+    for (const auto& a : A) {
+        s.push(a);
+    }
+    A.clear();
+    while (!s.empty()) {
+        A += s.top();
+        s.pop();
+    }
+    return A;
+}
